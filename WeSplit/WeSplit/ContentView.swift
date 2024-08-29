ec39fee484460318f390c1f963180a6df2a7e0b0
Alvaro <alvher.orellana@gmail.com>
2024-08-29 20:59:12 -0300

    Created project 1 for day 16

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Alvaro Orellana on 29-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var billAmount = 0.0
+    @State var numberOfPeople = 2
+    @State var tipPercentage = 0
+    @FocusState var amountTextFieldIsFocused: Bool
+    
+    let tipPercentages = [0, 10, 25, 50]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $billAmount, format: .currency(code:  Locale.current.currency?.identifier ?? "usd"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountTextFieldIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2...99, id: \.self) { number in
+                            Text("\(number)")
+                        }
+                    }
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0...100, id: \.self) { tipPercentage in
+                            Text(tipPercentage, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: "usd"))
+
+                }
+                Section("Total amount") {
+                    Text(total, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("We Split")
+            .toolbar {
+                if amountTextFieldIsFocused {
+                    Button("Done") {
+                        amountTextFieldIsFocused = false
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    var total: Double {
+        self.billAmount + (self.billAmount / 100 * Double(self.tipPercentage))
+    }
+    
+    var totalPerPerson: Double {
+        let tipAmount = billAmount / 100.0 * Double(tipPercentage)
+        return (billAmount + tipAmount) / Double(numberOfPeople)
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
