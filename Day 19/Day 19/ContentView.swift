f76d83fb56f6e518314eb8322a5887417646642a
Alvaro <alvher.orellana@gmail.com>
2024-09-25 19:07:49 -0300

    had not added files in previous commit

--- Day 19/Day 19/ContentView.swift
+++ Day 19/Day 19/ContentView.swift
@@ -8,16 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var unitsQuantity = ""
+    @State var inputUnit: LengthUnit = .meters
+    @State var outputUnit: LengthUnit = .meters
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Picker("Input unit", selection: $inputUnit) {
+                    ForEach(LengthUnit.allCases) { unit in
+                        Text(unit.rawValue)
+                    }
+                }
+                .pickerStyle(.menu)
+                TextField("Amount in input unit", text: $unitsQuantity)
+            }
+            Section("Output unit") {
+                Picker("Output unit", selection: $outputUnit) {
+                    ForEach(LengthUnit.allCases) { unit in
+                        Text(unit.rawValue)
+                    }
+                }
+                .pickerStyle(.menu)
+            }
+            if unitsQuantity.isEmpty {
+                Text("enter the input amount")
+            } else {
+                Text("\(unitsQuantity) \(inputUnit) is equal to \(convertUnits().formatted()) \(outputUnit)")
+            }
         }
-        .padding()
+    }
+    
+    private func convertUnits() -> Double {
+        LengthUnit.convert(from: inputUnit, to: outputUnit, quantity: Double(unitsQuantity) ?? 0)
     }
 }
+
+enum LengthUnit: String, Identifiable, CaseIterable {
+    case centimeters
+    case meters
+    case kilometers
+    case feet
+    case yards
+    case miles
+    
+    var id: Self {
+        self
+    }
+    
+    // Coefficient to convert from given unit to base unit (meters)
+    // Multiply the given value with this coefficient to convert to meters
+    private var ratioOfmetersPerUnit: Double {
+        switch self {
+        case .centimeters: 1 / 100
+        case .meters: 1 / 1
+        case .kilometers: 1 / 0.001
+        case .feet: 1 / 3.28084
+        case .yards: 1 / 1.09361
+        case .miles: 1 / 0.000539957
+        }
+    }
+    
+    static func convert(from sourceUnit: LengthUnit, to destinationUnit: LengthUnit, quantity: Double) -> Double {
+        let quantityInMeters = sourceUnit.ratioOfmetersPerUnit * quantity
+        let quantityInDestinationUnit = quantityInMeters / destinationUnit.ratioOfmetersPerUnit
+        return quantityInDestinationUnit
+    }
+}
+
 
 #Preview {
     ContentView()
--- Day 19/Day 19/Day_19App.swift
+++ Day 19/Day 19/Day_19App.swift
@@ -15,3 +15,4 @@ struct Day_19App: App {
         }
     }
 }
+
