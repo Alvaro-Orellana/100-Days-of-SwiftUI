282b8c05aeeaffa88acb389391ba1beaa62959a4
Alvaro <alvher.orellana@gmail.com>
2024-10-02 19:24:57 -0300

    DAY 28 DONEEEE

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -20,30 +20,52 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .center, spacing: 0) {
-                    Text("When do you want to wake up")
+                Section {
                     DatePicker("Please enter a time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
                         .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up")
                 }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
+                Section {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 2...10, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
                 }
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
+//                    Stepper("^[\(coffeeAmount) cup of coffee](inflect: true)", value: $coffeeAmount, in: 1...20)
+                    Picker("Cups of coffee per day", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) { number in
+                            Text("\(number)")
+                        }
+                    }
+                    Text(coffeeAmount.description)
+                } header: {
                     Text("Daily coffee intake")
-                    Stepper("^[\(coffeeAmount) cup of coffee](inflect: true)", value: $coffeeAmount, in: 1...20)
+                }
+                
+                Section {
+                    Text(alertTitle)
+                    Text(alertMessage)
+                        .font(.largeTitle.weight(.heavy))
                 }
             }
             .font(.headline)
             .padding()
             .navigationTitle("Better Rest")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
+            .onChange(of: sleepAmount) {
+                calculateBedTime()
             }
-            .alert(alertTitle, isPresented: $showAlert) {
-                Text(alertMessage)
-                Button("Ok", role: .cancel) {}
+            .onChange(of: coffeeAmount) {
+                calculateBedTime()
             }
+            .onChange(of: wakeUpTime) {
+                calculateBedTime()
+            }
+            .onAppear {
+                calculateBedTime()
+            }
+            
+
         }
     }
     
