42c0ae211d3d727df6904a5297214c8046ab35c1
Alvaro <alvher.orellana@gmail.com>
2024-10-09 22:36:55 -0300

    day 32

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,34 +10,33 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var animationAmount = 1.0
-    
+    @State private var rotationAmount = 0.0
     let title: String
     let color: Color
-
     
     var body: some View {
         Button(title) {
-            animationAmount += 1
+            withAnimation(.spring(duration: 3, bounce: 0.5)) {
+               rotationAmount += 360
+                
+            }
         }
-        .padding(50)
-        .foregroundStyle(.white)
-        .background(color)
-        .clipShape(.circle)
-        .overlay {
-            Circle()
-                .stroke(color)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 2).repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        }
-        
-        
+            .padding(50)
+            .foregroundStyle(.white)
+            .background(color)
+            .clipShape(.circle)
+            .rotation3DEffect(.degrees(rotationAmount), axis: (x: 0.3, y: 1, z: 0))
+//            .overlay {
+//                Circle()
+//                    .stroke()
+//                    .foregroundStyle(color)
+//                    .scaleEffect(animationAmount)
+//                    .opacity(2-animationAmount)
+//                    .animation(.easeInOut(duration: 2).repeatForever(), value: animationAmount)
+//                
+//            }
     }
 }
-
 
 
 #Preview {
