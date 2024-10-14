a7c7c724b3fdd1226403fcf356611bdd62b66193
Alvaro <alvher.orellana@gmail.com>
2024-10-14 15:36:07 -0300

    day 34 or 35

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     private static let countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     private static let numberOfFlagsPerChoice = 4
     private static let numberOfQuestions = 2
+    private static let flagWidth: CGFloat = 200
     
     @State private var countriesToShow: ArraySlice<String>
     @State private var correctAnswer: String
@@ -20,6 +21,7 @@ struct ContentView: View {
     @State private var presentAlert = false
     @State private var score = 0
     @State private var numberOfQuestions = Self.numberOfQuestions
+    @State var tappedFlagName = ""
     
     init() {
         let countriesToShow = Self.countries.shuffled().prefix(upTo: Self.numberOfFlagsPerChoice)
@@ -37,42 +39,27 @@ struct ContentView: View {
 
             VStack {
                 Text("Guess the flag")
-                    .foregroundStyle(.white)
                     .font(.largeTitle.bold())
-                
-                Spacer()
-                
-                VStack(spacing: 20) {
-                    VStack {
-                        Text("Tap the flag of")
-                            .font(.subheadline)
-                            .fontWeight(.heavy)
-                        Text(correctAnswer.capitalized)
-                            .font(.largeTitle)
-                            .fontWeight(.semibold)
-                    }
-                    .foregroundStyle(.white)
-                    
-                    ForEach(countriesToShow, id: \.self) { country in
-                        Button {
-                            flagTapped(selectedFlag: country)
-                        } label: {
-                            FlagImage(imageName: country)
-                        }
-                    }
+                                
+                VStack {
+                    Text("Tap the flag of")
+                        .font(.subheadline.weight(.heavy))
+                    Text(correctAnswer.capitalized)
+                        .font(.largeTitle.weight(.semibold))
+
+                    flags
+//                        .frame(width: 200)
+                        .frame(maxWidth: 200, minHeight: 100)
                 }
+                .padding()
                 .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
                 .background(.regularMaterial)
                 .clipShape(.rect(cornerRadius: 20))
                 
-                Spacer()
-                
                 Text("Score is \(score)")
                     .titleStyle()
-//                    .font(.title2.bold())
-//                    .foregroundStyle(.white)
             }
+            .foregroundStyle(.white)
             .padding()
         }
         .alert(alertTitle, isPresented: $presentAlert) {
@@ -80,7 +67,46 @@ struct ContentView: View {
         } message: {
             Text(alertMesage)
         }
-           
+    }
+    
+    private var flags: some View {
+        ForEach(countriesToShow, id: \.self) { country in
+            Button {
+                flagTapped(selectedFlag: country)
+            } label: {
+                GeometryReader { geometryProxy in
+                    FlagImage(imageName: country)
+                        .opacity(calculateOpacity(countryName: country))
+                        .animation(.default.delay(0.2), value: presentAlert)
+                    
+                        
+                    
+//                        .scaleEffect(calculateScale(countryName: country, size: geometryProxy.size))
+                        .rotation3DEffect(calculateDegrees(countryName: country), axis: (x: 0, y: 1, z: 0))
+                        .animation(.snappy(duration: 2, extraBounce: 0.6).delay(0.1), value: presentAlert)
+                        .border(.blue, width: 2)
+                }
+                .border(.red, width: 2)
+                
+            }
+        }
+    }
+    
+//    private func calculateScale(countryName: String, size: CGSize) -> CGSize {
+//        print(size)
+//        if tappedFlagName != countryName && !tappedFlagName.isEmpty {
+//            return CGSize(width: size.width * 0.5, height: size.height * 0.5)
+//        } else {
+//            size
+//        }
+//    }
+    
+    private func calculateOpacity(countryName: String) -> Double {
+        tappedFlagName == countryName || tappedFlagName.isEmpty ? 1 : 0.25
+    }
+    
+    private func calculateDegrees(countryName: String) -> Angle {
+        Angle.degrees(tappedFlagName == countryName && !tappedFlagName.isEmpty ? 360 : 0)
     }
     
     private var gameIsOver: Bool { self.numberOfQuestions == 0 }
@@ -88,11 +114,12 @@ struct ContentView: View {
     private func flagTapped(selectedFlag: String) {
         numberOfQuestions -= 1
         presentAlert = true
+        tappedFlagName = selectedFlag
         
         if selectedFlag == correctAnswer {
             score += 1
             alertTitle = "Correct"
-            alertMesage = "Your score is \(score)"
+            alertMesage = "Your score is \(score)"  
         } else {
             alertTitle = "Incorrect"
             alertMesage = "That's the flag of \(selectedFlag)"
@@ -107,6 +134,7 @@ struct ContentView: View {
     private func newQuestion() {
         countriesToShow = Self.countries.shuffled().prefix(upTo: Self.numberOfFlagsPerChoice)
         correctAnswer = countriesToShow.randomElement()!
+        tappedFlagName = ""
     }
     
     private func restartGame() {
@@ -126,11 +154,11 @@ struct FlagImage: View {
     }
 }
 
-struct TitleStyle: ViewModifier{
+struct TitleStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
             .font(.largeTitle)
-            .foregroundStyle(.blue)
+            .foregroundStyle(.white)
     }
 }
 
